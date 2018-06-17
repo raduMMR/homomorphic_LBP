f99eb6c28b86706e72e0be75e47b38cebf6c9ea9
raduMMR <radu.mardari12@gmail.com>
2018-06-17 13:08:57 +0300

    added file saving of the CTXTs to prevent memory exhaustion

--- HE_FR_LBP.cpp
+++ HE_FR_LBP.cpp
@@ -2,101 +2,178 @@
 #include "HE_FR_LBP.h"
 #include "basic_primitives.h"
 #include "he_lbp.h"
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define NUMBER_OF_PIXELS 1024
+void add1Bit(vector<Ctxt*> &metric, Ctxt* &ctxt) {
+    Ctxt *carry = encryptBitVal(vector<long>(NSLOTS, 0));
+    *carry = (*metric[0]);
+    carry->multiplyBy((*ctxt));
 
-void EncRegion::cloneRegion(const EncRegion& copy){
-    for(int i=0; i<copy.enc_pixels.size(); i++){
-        Ctxt *c = encryptBitVal(vector<long>(nslots, 0));
-        *c = *copy.enc_pixels[i];
-        this->enc_pixels.push_back(c);
+    metric[0]->addCtxt(*ctxt);
+
+    Ctxt *new_carry = encryptBitVal(vector<long>(NSLOTS, 0));
+    for(int i=1; i<metric.size(); i++) {
+        *new_carry = (*metric[i]);
+        new_carry->multiplyBy(*carry);
+        metric[i]->addCtxt(*carry);
+        // TO DO: use this costly refresh only when mandatory.
+        pseudoRefreshCtxt(metric[i]);
+        *carry = *new_carry;
     }
 
-    cout << "Passed\n";
+    delete carry;
+    delete new_carry;
+}
 
-    this->enc_neighbours = vector<vector<Ctxt*>>(copy.enc_neighbours.size());
-    for(int i=0; i<copy.enc_neighbours.size(); i++){
-        for(int j=0; j<copy.enc_pixels.size(); j++){
-            Ctxt *c = encryptBitVal(vector<long>(nslots, 0));
-            *c = *copy.enc_neighbours[i][j];
-            this->enc_neighbours[i].push_back(c);
+vector<Ctxt*> computeRegionMetric(char *histFile1, char *histFile2){
+    fstream hist1(histFile1, fstream::in);
+    fstream hist2(histFile2, fstream::in);
+
+    // 16 is chosen arbitrarly, hope it will suffice and at the same time is not too big.
+    vector<Ctxt*> metric(16, encryptBitVal(vector<long>(NUMBER_OF_PIXELS, 0)));
+
+    Ctxt* c1 = encryptBitVal(vector<long>(NUMBER_OF_PIXELS, 0));
+    Ctxt* c2 = encryptBitVal(vector<long>(NUMBER_OF_PIXELS, 0));
+
+    for(int i=0; i<256; i++){
+        hist1 >> *c1;
+        hist2 >> *c2;
+
+        // compare the two amplitudes of the same LBP code.
+        c2->addCtxt(*c1);
+        // adding one amplitude to another, we obtain the number of differences
+
+
+        // now add the differences to the global metric.
+        add1Bit(metric, c2);
+    }
+
+    delete c1; delete c2;
+
+    hist1.close();
+    hist2.close();
+
+    return metric;
+}
+
+vector<Ctxt*> getRow(const vector<Ctxt*> &global_metric, const int slot_index){
+    vector<long> plain_mask(NUMBER_OF_PIXELS, 0);
+    plain_mask[slot_index] = 1;
+    // we will have the value 1 in slot 1 of each Ctxt so that I can obtain
+    // each row of Ctxts and add to the final metric.
+    vector<Ctxt*> row(global_metric.size(), encryptBitVal(plain_mask));
+
+    // I will zeroes everywhere except in every slot_index of the Ctxt.
+    for(int i=0; i<global_metric.size(); i++){
+        row[i]->multiplyBy(*global_metric[i]);
+    }
+    
+    // shift the row in the front position to add the returned value
+    // to the final metric.
+    for(int i=0; i<row.size(); i++){
+        // shift Ctxt toward last slot.
+        ea->shift(*row[i], NUMBER_OF_PIXELS-slot_index-1);
+    }
+
+    return row;
+}
+
+void addRow(vector<Ctxt*> &global_metric, vector<Ctxt*> &row){
+    for(int i=0; i<row.size(); i++){
+        add1Bit(global_metric, row[i]);
+    }
+}
+
+vector<Ctxt*> compareTwoImages(char **histogramFiles1, char **histogramFiles2, int nb_regions){
+    vector<Ctxt*> global_metric = encryptIntVal(vector<long>(NUMBER_OF_PIXELS, 0), 16);
+
+    for(int i=0; i<nb_regions; i++){
+        vector<Ctxt*> region_metric = computeRegionMetric(histogramFiles1[i], histogramFiles2[i]);
+        hom_binarySum(global_metric, region_metric);
+    }
+
+    // accumulate in the last slot the global metric from partial metric of the region metrics
+    
+    // get all rows, except last one where the accumulation will take place.
+    for(int i=0; i<NUMBER_OF_PIXELS-1; i++){
+        vector<Ctxt*> row = getRow(global_metric, i);
+        addRow(global_metric, row);
+        for(int j=0; j<row.size(); j++){
+            delete row[j];
         }
     }
 
+    return global_metric;
 }
 
-EncRegion::~EncRegion(){
+void ImageProcessor::computeNbOfOccurences(vector<Ctxt*> LBP_codes, vector<long> histogram, char *filename){
+    // save the histogram of this region in this file
+    fstream localHistFile(filename, fstream::out|fstream::trunc);
+    assert(localHistFile.is_open());
+
+    for(int i=0; i<256; i++){
+        vector<Ctxt*> lbp_code = encryptIntVal(vector<long>(NUMBER_OF_PIXELS,/*LBP_code=*/ i), /*T_BITS=*/8);
+
+        Ctxt *eh = compute_z(0, 8, lbp_code, LBP_codes);
+
+        for(int j=0; j<lbp_code.size(); j++){
+            delete lbp_code[j];
+        }
+
+        localHistFile << *eh << endl;
+        
+        // testing with the plain histogram.
+        // vector<long> dec_hist = decryptBitVal(eh);
+        delete eh;
+
+        // int hist_val = 0;
+        // for(int j=0; j<NUMBER_OF_PIXELS; j++){
+        //     hist_val += dec_hist[j];
+        // }
+        // if(hist_val != histogram[i]){
+        //     cout << "Hist diff " << i << endl;
+        // } else { cout << "Step " << i << " completed.\n"; }
+    }
+
+    localHistFile.close();
+}
+
+void ImageProcessor::encryptRegion(vector<long> pixels, vector<vector<long>> neighbours, char *regionFile){
+    assert(neighbours.size() == 8);
+
+    fstream regionStream(regionFile, fstream::out|fstream::trunc);
+
+    // EncRegion* er = new EncRegion(this->nslots);
+    // er->enc_pixels = encryptIntVal(pixels, 8);
+    // er->enc_neighbours = vector<vector<Ctxt*>>(8);
+    // for(int i=0; i<8; i++) {
+    //     er->enc_neighbours[i] = encryptIntVal(neighbours[i], 8);
+    // }
+
+    vector<Ctxt*> enc_pixels = encryptIntVal(pixels, 8);
     for(int i=0; i<enc_pixels.size(); i++){
+        regionStream << *enc_pixels[i] << endl;
         delete enc_pixels[i];
     }
 
-    for(int i=0; i<enc_neighbours.size(); i++){
-        for(int j=0; j<enc_neighbours[i].size(); j++){
-            delete enc_neighbours[i][j];
+    for(int i=0; i<8; i++){
+        vector<Ctxt*> enc_neighbour = encryptIntVal(neighbours[i], 8);
+        for(int j=0; j<enc_neighbour.size(); j++){
+            regionStream << *enc_neighbour[j] << endl;
+            delete enc_neighbour[j];
         }
     }
 
-    cout << "Called EndRegion destructor\n";
-}
-
-EncHistogram::EncHistogram(){
-    enc_hist = vector<Ctxt*>(256);
-    // all lbp_codes from 0 to 255.
-}
-
-EncHistogram::~EncHistogram(){
-    for(int i=0; i<enc_hist.size(); i++){
-            delete enc_hist[i];
-    }
-}
-
-void EncHistogram::computeNbOfOccurences(vector<Ctxt*> LBP_codes, vector<long> histogram){
-    for(int i=0; i<256; i++){
-	vector<Ctxt*> lbp_code = encryptIntVal(vector<long>(NUMBER_OF_PIXELS,/*LBP_code=*/ i), /*T_BITS=*/8);
-
-        //enc_hist[i] = compute_z(0, 8, lbp_code, LBP_codes);
-
-	Ctxt *eh = compute_z(0, 8, lbp_code, LBP_codes);
-
-	for(int j=0; j<lbp_code.size(); j++){
-		delete lbp_code[j];
-	}
-	
-	vector<long> dec_hist = decryptBitVal(eh);
-	delete eh;
-
-	int hist_val = 0;
-	for(int j=0; j<NUMBER_OF_PIXELS; j++){
-		hist_val += dec_hist[j];
-	}
-	if(hist_val != histogram[i]){
-		cout << "Hist diff " << i << endl;
-	} else { cout << "Step " << i << " completed.\n"; }
-    }
-}
-
-EncRegion* ImageProcessor::encryptRegion(vector<long> pixels, vector<vector<long>> neighbours){
-    assert(neighbours.size() == 8);
-
-    EncRegion* er = new EncRegion(this->nslots);
-    er->enc_pixels = encryptIntVal(pixels, 8);
-    er->enc_neighbours = vector<vector<Ctxt*>>(8);
-    for(int i=0; i<8; i++) {
-        er->enc_neighbours[i] = encryptIntVal(neighbours[i], 8);
-    }
-
-    return er;
+    regionStream.close();
+    regionStream.flush();
 }
 
 // compute the LBP codes in the clear for comparison.
 vector<long> clearLBPcodes(vector<long> pixels, vector<vector<long>> neighbours){
-<<<<<<< HEAD
-    vector<long> lbp_codes(1024);
-    for(int i=0; i<1024; i++) {
-=======
     vector<long> lbp_codes(NUMBER_OF_PIXELS);
     for(int i=0; i<NUMBER_OF_PIXELS; i++) {
->>>>>>> be047bb8c648a38be90f741d91f4759e27dfc54f
         lbp_codes[i] = 0;
         for(int j=0; j<8; j++) {
             lbp_codes[i] |= (neighbours[j][i] >= pixels[i]) << j;
@@ -122,9 +199,9 @@ vector<long> computeClearHistogram(vector<long> lbp_codes){
     return histogram;
 }
 
-void ImageProcessor::encryptImage(uint8_t **image){
+void ImageProcessor::encryptImage(uint8_t **image, char **regionFiles){
     assert(this->nslots == NUMBER_OF_PIXELS);
-    // for an image of size 256x256 we split it in 16 regions, 4x4
+    // for an image of size 256x256 we split it in 64 regions, 4x4
     // such that a ctxt contain the pixels from an entire region, 1024
 
     // we added two extra columns and two extra lines to border 
@@ -154,63 +231,94 @@ void ImageProcessor::encryptImage(uint8_t **image){
 
             // encrypt the region
             cout << "se cripteaza regiunea (" << i << ", " << j << ")\n";
-            EncRegion *er = encryptRegion(pixels, neighbours);
-            this->encrypted_regions.push_back(er);
+            encryptRegion(pixels, neighbours, regionFiles[i*8+j]);      
             cout << "Regiune criptata\n";
-
-            // compare the LBP codes.
-            vector<long> plain_lbp = clearLBPcodes(pixels, neighbours);
-
-            cout << "Se calculeaza codurile LBP pentru regiune ...\n";
-            vector<Ctxt*> LBP_codes = computeLBP4Region(0);
-            vector<long> dec_lbp = decryptIntVal(LBP_codes);
-            assert( dec_lbp.size() >= plain_lbp.size() );
-            for(int z=0; z<plain_lbp.size(); z++){
-                if(plain_lbp[z] != dec_lbp[z]){
-                    cout << "Coduri LBP diferite.\n";
-			cout << "Index = " << z << ", plain = " << plain_lbp[z] << ", enc = " << dec_lbp[z] << endl;
-                    break;
-                }
-            }
-            cout << "Coduri LBP calculate.\n";
-		delete er;
-            // compare the two histograms.
-            cout << "Se compara histogramele ...\n";
-            vector<long> plain_hist = computeClearHistogram(plain_lbp);
-	cout << "clear histogram done\n";
-
-            EncHistogram eh;
-            eh.computeNbOfOccurences(LBP_codes, plain_hist);
-            assert(plain_hist.size() == 256);
-           /* for(int z=0; z<plain_hist.size(); z++){
-                if(plain_hist[z] != decryptBitVal(eh.enc_hist[z])[0] ){
-                    cout << "Histograme diferite\n";
-                }
-            }*/
-            cout << "Histograme comparate.\n";
-
-            // cleanup.
-            for(int z=0; z<LBP_codes.size(); z++){
-                delete LBP_codes[z];
-            }
-
-            break;
         }
     }
 }
 
-vector<Ctxt*> ImageProcessor::computeLBP4Region(int region_id){
-    return hom_LBP(encrypted_regions[region_id]->enc_pixels, encrypted_regions[region_id]->enc_neighbours, 8);
+vector<Ctxt*> ImageProcessor::computeLBP4Region(char *regionFile){
+    fstream regionStream(regionFile, fstream::in);
+    vector<Ctxt*> enc_pixels = encryptIntVal(vector<long>(NUMBER_OF_PIXELS, 0), 8);
+    vector<vector<Ctxt*>> enc_neighbours(8, enc_pixels);
+
+    for(int i=0; i<8; i++){
+        regionStream >> *enc_pixels[i];
+    }
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            regionStream >> *enc_neighbours[i][j];
+        }
+    }
+
+    regionStream.close();
+
+    vector<Ctxt*> enc_lbp = hom_LBP(enc_pixels, enc_neighbours, 8);
+
+    // cleanup.
+    for(int i=0; i<enc_pixels.size(); i++){
+        delete enc_pixels[i];
+    }
+    for(int i=0; i<enc_neighbours.size(); i++){
+        for(int j=0; j<enc_neighbours[i].size(); j++){
+            delete enc_neighbours[i][j];
+        }
+    }
+
+    return enc_lbp;
 }
 
-EncHistogram ImageProcessor::computeHistogram4Region(int region_id){
-    vector<Ctxt*> LBP_codes = computeLBP4Region(region_id);
+void ImageProcessor::computeHistogram4Region(char *regionFile, char *regionHist){
+    vector<Ctxt*> LBP_codes = computeLBP4Region(regionFile);
 
-    EncHistogram eh;
+    computeNbOfOccurences(LBP_codes, vector<long>(NUMBER_OF_PIXELS, 0), regionHist);
+}
 
-    eh.computeNbOfOccurences(LBP_codes, vector<long>(NUMBER_OF_PIXELS, 0));
+void ImageProcessor::computeHistogram(char **regionFiles,  char **histFiles){
 
-    return eh;
+    for(int i=0; i<NUMBER_OF_REGIONS; i++){
+        this->computeHistogram4Region(regionFiles[i], histFiles[i]);
+    }
+    
+}
+
+void myitoa(int number, char *buffer){
+    strcpy(buffer, "");
+
+    vector<long> digits;
+    do{
+        digits.push_back(number%10);
+        number = number/10;
+    }while(number!=0);
+
+    for(int i=digits.size()-1; i>=0; i--){
+        switch(digits[i]){
+            case 0: 
+                strcat(buffer, "0"); break;
+            case 1: 
+                strcat(buffer, "1"); break;
+            case 2: 
+                strcat(buffer, "2"); break;
+            case 3: 
+                strcat(buffer, "3"); break;
+            case 4: 
+                strcat(buffer, "4"); break;
+            case 5: 
+                strcat(buffer, "5"); break;
+            case 6: 
+                strcat(buffer, "6"); break;
+            case 7: 
+                strcat(buffer, "7"); break;
+            case 8: 
+                strcat(buffer, "8"); break;
+            case 9: 
+                strcat(buffer, "9"); break;
+            default: 
+                cout << "Can't be! Digit not existing ??? \n"; 
+                exit(-1);
+        }
+    }
 }
 
 void test_HE_FR_LBP(){
@@ -230,23 +338,44 @@ void test_HE_FR_LBP(){
         image[0][i] = 0;
     }
 
+    char **regionFiles = new char*[NUMBER_OF_REGIONS];
+    char **histFiles = new char*[NUMBER_OF_REGIONS];
+    for(int i=0; i<NUMBER_OF_REGIONS; i++){
+        regionFiles[i] = new char[7];
+        myitoa(i, regionFiles[i]);
+        strcat(regionFiles[i], ".enc");
+
+        histFiles[i] = new char[8];
+        myitoa(i, histFiles[i]);
+        strcat(histFiles[i], ".hist");
+    }
+
     ImageProcessor ip;
-    if(NSLOTS >= 1024)
-        ip.setNumberOfSlots(NUMBER_OF_PIXELS);
+    ip.setNumberOfSlots(NUMBER_OF_PIXELS);
 
-    // cout << "Se cripteaza imaginea ...\n";
-    ip.encryptImage(image);
-    // cout << "Imagine criptata.\n";
+    ip.encryptImage(image, regionFiles);
 
-    // cout << "Se calculeaza histogramele ...\n";
-    // for(int i=0; i<64; i++){
-        // EncHistogram enc_hist = ip.computeHistogram4Region(0);
-    // }
-    // cout << "Histograme calculate.\n";
+    ip.computeHistogram(regionFiles, histFiles);
+
+    vector<Ctxt*> result = compareTwoImages(histFiles, histFiles, NUMBER_OF_REGIONS);
+
+    vector<long> decision = decryptIntVal(result);
+    cout << "Metrica comparatiei histogramelor: " << decision[NUMBER_OF_PIXELS-1] << endl;
 
     // cleanup, delete image matrix.
+    for(int i=0; i<result.size(); i++){
+        delete result[i];
+    }
+
     for(int i=0; i<258; i++){
         delete image[i];
     }
     delete image;
+
+    for(int i=0; i<NUMBER_OF_REGIONS; i++){
+        delete regionFiles[i];
+        delete histFiles[i];
+    }
+    delete regionFiles;
+    delete histFiles;
 }
--- HE_FR_LBP.h
+++ HE_FR_LBP.h
@@ -5,83 +5,34 @@
 #include "./../FHE.h"
 using namespace std;
 
-
-// we choose to make EncRegion and NumberOfOccurences classes to use 
-// the destructor method of the objects. TO DO: protect more the fields of these 
-// two classes
-class EncRegion
-{
-    int nslots;
-
-    void cloneRegion(const EncRegion& copy);
-
-public:
-    vector<Ctxt*> enc_pixels;
-    vector<vector<Ctxt*>> enc_neighbours;
-
-    EncRegion(int nslots){
-        this->nslots = nslots;
-    }
-
-    EncRegion(const EncRegion &copy){
-        cloneRegion(copy);
-    }
-
-    EncRegion& operator=(const EncRegion& copy){
-        cloneRegion(copy);
-    }
-
-    ~EncRegion();
-};
-
-class EncHistogram{
-public:
-    EncHistogram();
-
-// number of occurences for each code LBP. enc_hist[i] - nb of occurence for code i, i=0..255
-    vector<Ctxt*> enc_hist;
-
-    // for each lbp code from enc_hist compute the number of occurences.
-    // @param lbp_codes is matrix
-    /*          bit_0 bit_1  ... bit_7
-    lbp_codes = [                    ]  slot_0    => LBP code for pixel_0 
-                        ...             slot_1
-                        ...              ...
-                [                    ]  slot_4096  => LBP code for pixel_4096
-    */
-    void computeNbOfOccurences(vector<Ctxt*> lbp_codes, vector<long> clear_hist);
-
-    ~EncHistogram();
-};
+#define NUMBER_OF_REGIONS 64 // the number of regions the image is divided by
+#define NUMBER_OF_PIXELS 1024 // the number of pixels in each region
 
 class ImageProcessor{
 
     int nslots;
 
-    vector<EncRegion*> encrypted_regions;
-
     void encodeImage(uint8_t **image);
 
-    vector<Ctxt*> computeLBP4Region(int region_id);
+    vector<Ctxt*> computeLBP4Region(char *regionFile);
 
-    EncRegion *encryptRegion(vector<long> pixels, vector<vector<long>> neighbours);
+    void encryptRegion(vector<long> pixels, vector<vector<long>> neighbours, char *regionFile);
+
+    void computeHistogram4Region(char *regionFile, char *regionHist);
+
+    void computeNbOfOccurences(vector<Ctxt*> LBP_codes, vector<long> histogram, char *filename);
 
 public:
 
-    void setNumberOfSlots(int nslots){
-        this->nslots = nslots;
-    }
+    void setNumberOfSlots(int nslots){ this->nslots = nslots; }
 
-    void encryptImage(uint8_t **image);
+    void encryptImage(uint8_t **image, char **regionFiles);
 
-    EncHistogram computeHistogram4Region(int region_id);
-
-    ~ImageProcessor(){
-        for(int i=0; i<encrypted_regions.size(); i++){
-            delete encrypted_regions[i];
-        }
-    }
+    void computeHistogram(char **regionFiles, char **histFiles);
 };
 
+// compare two encrypted histograms of two images
+// for each region of an image, the local histogram is saved in one file.
+vector<Ctxt*> compareTwoImages(char **histogram1Files, char **histogram2Files, int nb_regions);
 
 void test_HE_FR_LBP();
